7c457c59efab28d4990535c47724d99bbcec383a
auraviaco <rampadinesh4@@gmail.com>
2026-03-20 22:28:31 +0530

    sample change

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from langchain_ollama import ChatOllama
 model = ChatOllama(model="mistral",
                    temperature=0.5)
 
-result=model.invoke("about elon musk in 10 words")
+result=model.invoke("explain two pointer approaxch with example")
 print(result.content)
