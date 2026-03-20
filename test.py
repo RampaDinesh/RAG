e9d6d9d73f0de8e3fc5fd441f249e4f7aff2c372
RampaDinesh <=>
2026-03-20 22:33:29 +0530

    fixed spelling mistake of approach

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from langchain_ollama import ChatOllama
 model = ChatOllama(model="mistral",
                    temperature=0.5)
 
-result=model.invoke("explain two pointer approaxch with example")
+result=model.invoke("explain two pointer approach with example")
 print(result.content)
